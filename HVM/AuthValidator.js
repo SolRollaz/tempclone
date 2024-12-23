af6a9b89699987aaee2bfb0bac7794570e14cb07
SolRollaz <customai.nz@gmail.com>
2024-12-23 18:37:10 +1300

    Update AuthValidator.js

--- HVM/AuthValidator.js
+++ HVM/AuthValidator.js
@@ -1,6 +1,6 @@
 const { WalletClient } = require("dag4");
 const ethers = require("ethers");
-const SystemConfig = require("./SystemConfig");
+const SystemConfig = require("../SystemConfig");
 
 class AuthValidator {
     constructor() {
