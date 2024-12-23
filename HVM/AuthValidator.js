e9bf11580de701e8bbcfc434534ca51a83394345
SolRollaz <customai.nz@gmail.com>
2024-12-23 22:37:05 +1300

    Update AuthValidator.js

--- HVM/AuthValidator.js
+++ HVM/AuthValidator.js
@@ -1,4 +1,4 @@
-const { WalletClient } = require("dag4");
+const { WalletClient } = require("@stardust-collective/dag4");
 const ethers = require("ethers");
 const SystemConfig = require("../SystemConfig");
 
